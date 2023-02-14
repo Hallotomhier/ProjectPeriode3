ea90add569f19274b2fce6e002a60fbd99c6fa91
Hallotomhier <tomwinkel2005@gmail.com>
2023-02-14 13:39:56 +0100

    Added Movement

---
+++ ProjectPeriode3/Assets/Script/MovementScripts/MouseMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float mouseX;
+    public float rotateSpeed;
+    public float mouseY;
+    public Transform player;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+        transform.Rotate(-mouseY,0,0);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ ProjectPeriode3/Assets/Script/MovementScripts/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float horizontal;
+    public float vertical;
+    public Vector3 movement;
+    public float movementSpeed = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical  = Input.GetAxis("Vertical");
+        movement.x = horizontal;
+        movement.z = vertical;
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
+    }
+}
