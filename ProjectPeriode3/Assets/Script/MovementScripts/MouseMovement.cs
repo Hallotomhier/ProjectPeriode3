600bf100eff9659670431470d5b96f777e8ec8b6
Hallotomhier <tomwinkel2005@gmail.com>
2023-03-29 13:35:52 +0200

    Clamp toegevoegd aan camera

--- ProjectPeriode3/Assets/Script/MovementScripts/MouseMovement.cs
+++ ProjectPeriode3/Assets/Script/MovementScripts/MouseMovement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    public float mousSens, mouseY, mouseX;
-    public Vector3 dir;
+    public float mousSens, mouseY, mouseX ,rotY;
+    public Vector3 dir , clampY;
     public GameObject playerBody;
+    
 
     void Start()
     {
@@ -19,13 +20,19 @@ public class MouseMovement : MonoBehaviour
         mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mousSens;
         mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mousSens;
 
-        mouseY = Mathf.Clamp(mouseY, -90, 90);
-
         dir = new Vector3(0, mouseX, 0);
         playerBody.transform.Rotate(dir * mousSens * Time.deltaTime);
 
         dir = new Vector3(-mouseY, 0, 0);
         transform.Rotate(dir * mousSens * Time.deltaTime);
+
+        rotY += mouseY;
+        rotY = Mathf.Clamp(rotY, -90, 90);
+        clampY = transform.eulerAngles;
+        clampY.x = -rotY;
+        transform.eulerAngles = clampY;
+
+
 
 
 
